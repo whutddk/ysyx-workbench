9ea1ad0cfa137b6b45dc7e3ceccb7a9dbdf4a4db
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-06 17:42:41 +0800

    >  compile NEMU 0121311371103 Ruige_Lee Linux DESKTOP-9M9APGT 4.4.0-22000-Microsoft #653-Microsoft Wed Apr 27 16:06:00 PST 2022 x86_64 x86_64 x86_64 GNU/Linux  17:42:41 up  8:27,  0 users,  load average: 0.52, 0.58, 0.59

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -207,10 +207,12 @@ word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
-  }
-
+  } else {
   /* TODO: Insert codes to evaluate the expression. */
   
+  *success = true;
+  return eval(0, nr_token-1);    
+  }
 
-  return eval(0, nr_token-1);
+
 }
